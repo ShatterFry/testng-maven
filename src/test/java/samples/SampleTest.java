ae74165e9e66624c2c373202762467d548276791
Nikita Kaverin <nikwhite333@gmail.com>
2018-04-01 14:24:04 +0300

    sample test

---
+++ src/test/java/samples/SampleTest.java
@@ -0,0 +1,22 @@
+package samples;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test(groups = {"unit"})
+    public void first(){
+        System.out.println("unit");
+    }
+
+    @Test(groups = {"web"})
+    public void second(){
+        System.out.println("web");
+        Assert.assertEquals("2", "1");
+    }
+
+    @Test(groups = {"db"})
+    public void third(){
+        System.out.println("db");
+    }
+}
